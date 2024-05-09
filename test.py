ff8bc7c39875a6e9a493e47f74523eef40ef04b1
Prajwal-18MD <prajju.18gryphon@gmail.com>
2024-05-09 17:29:08 +0530

    predict

--- test.py
+++ test.py
@@ -11,7 +11,17 @@ data = data[["G1","G2","G3","studytime","failures","absences"]]
 
 predict = "G3"
 
-X = np.array(data.drop([predict], 1))
+X = np.array(data.drop([predict], axis=1))
 Y = np.array(data[predict])
 
-x_train, y_train, x_test, y_test = sklearn.model_selection.train_test_split(X, Y, test_size = 0.1)
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size = 0.1)
+
+linear = linear_model.LinearRegression()
+
+linear.fit(x_train, y_train)
+acc = linear.score(x_test, y_test)
+print(acc)
+
+predictions = linear.predict(x_test)
+for x in range(len(predictions)):
+    print(predictions[x] , x_test[x], y_test[x]) 
