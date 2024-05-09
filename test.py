9dde8c10068e7441e79ee3d193624f5f3bf3fc66
Prajwal-18MD <prajju.18gryphon@gmail.com>
2024-05-10 00:26:45 +0530

    model save

--- test.py
+++ test.py
@@ -5,6 +5,9 @@ import numpy as np
 import sklearn
 from sklearn import linear_model
 from sklearn.utils import shuffle
+import matplotlib.pyplot as pyplot
+import pickle
+from matplotlib import style
 
 data = pd.read_csv("student-mat.csv" ,sep=";")
 data = data[["G1","G2","G3","studytime","failures","absences"]]
@@ -13,14 +16,30 @@ predict = "G3"
 
 X = np.array(data.drop([predict], axis=1))
 Y = np.array(data[predict])
-
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size = 0.1)
 
-linear = linear_model.LinearRegression()
+'''
+best = 0
+for _i in range(30):
 
-linear.fit(x_train, y_train)
-acc = linear.score(x_test, y_test)
-print(acc)
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size = 0.1)
+
+    linear = linear_model.LinearRegression()
+
+    linear.fit(x_train, y_train)
+    acc = linear.score(x_test, y_test)
+    print(acc)
+    
+    if acc > best:
+        best = acc
+    with open("studentmodel.pickle", "wb") as f:
+        pickle.dump(linear, f)'''
+    
+pickle_in = open("studentmodel.pickle", "rb")
+linear = pickle.load(pickle_in)
+
+print('Coefficient: \n', linear.coef_)
+print('Intercept: \n', linear.intercept_)
 
 predictions = linear.predict(x_test)
 for x in range(len(predictions)):
